4f5a18e97d542a53a4442b02e81bd373d6d8001d
Greg155 <gregakhun@gmail.com>
2020-10-12 10:00:39 +0200

    New update App.js + components folder (Navbar+QuoteCard)

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+            <a href="https://en.wikipedia.org/wiki/The_Simpsons">Simpsons's quotes!</a> YES!
+        </nav>
+    )
+}
+
+export default Navbar;
